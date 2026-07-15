846e13592df43ffb49c9ed04ffaf609ac9c35f2d
Rohan-Saxena644 <rohsax83@gmail.com>
2026-07-15 18:38:27 +0530

    Dockerize the app

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-        MONGODB_URL: process.env.MONGODB_URL,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    },
+    output: 'standalone',
     images: {
         remotePatterns: [
             {
